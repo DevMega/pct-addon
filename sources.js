06a3d7f5a360457a0d45b11472ba38267f020a4d
csong95 <csong95@users.noreply.github.com>
2017-12-08 21:56:30 -0600

    Update API source.

--- sources.js
+++ sources.js
@@ -1,10 +1,10 @@
 module.exports = { 
   // must point to /shows/ endpoint
   eztv: [
-  	"https://popcorntime.sh/api/eztv/shows/", 
+  	"https://tv-v2.api-fetch.website/", 
   	"https://popcornwvnbg7jev.onion.to/shows/"
   ],
 
   // must point to /api/v2/list_movies.json
-  yts: ["https://yts.ag/api/v2/list_movies.json" /*, "http://api.torrentsapi.com/api/v2/list_movies.json"*/ ] // torrents api might not be compat
+  yts: ["https://movies-v2.api-fetch.website/" /*, "http://api.torrentsapi.com/api/v2/list_movies.json"*/ ] // torrents api might not be compat
 }
